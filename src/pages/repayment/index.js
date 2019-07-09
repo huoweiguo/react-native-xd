af95587a987a9ca4c7813ff974e0dc6fb6de76ea
huoweiguo <592871458@qq.com>
2019-07-09 11:11:38 +0800

    add repay

--- src/pages/repayment/index.js
+++ src/pages/repayment/index.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Modal, ScrollView} from 'react-native';
 import { styles } from './styleCss';
 
 class Repayment extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            visible:  true
+        }
+    }
+
+    selectCard () {
+        console.log("select Card");
+    }
+
+    addCard () {
+        console.log('add Card');
+    }
+
     render () {
         return (
             <View style={styles.container}>
@@ -17,6 +32,46 @@ class Repayment extends Component {
                         <Image style={styles.r_arrow} source={require('../../assets/right-arrow.png')} />
                     </View>
                 </View>
+
+                <Modal visible={this.state.visible} transparent={true}>
+                    <View style={styles.mask_content}></View>
+    
+                    <View style={styles.popBox}>
+                        <View style={styles.sel_nav}>
+                            <Text style={styles.text1}>选择支付方式</Text>
+                            <Text style={styles.closeBtn}><Image style={styles.selImg} source={require('../../assets/delete.png')}/></Text>
+                        </View>
+
+                        <ScrollView style={{flex: 1}}>
+                            {/**card list render */}
+                            <View style={styles.bank_list}>
+                                <Text onPress={this.selectCard.bind(this)} style={styles.mask_event}></Text>
+                                <View style={styles.bank_left}>
+                                    <Image style={styles.bankImg_list} source={require('../../assets/icon_bank_logo.png')} />
+                                    <View>
+                                        <Text style={styles.bank_name_number}>中国工商银行储蓄卡  (4488)</Text>
+                                        <Text style={styles.bank_des}>银行单笔限额10000.00元</Text>
+                                    </View>
+                                </View>
+                                <Image style={styles.select_icon} source={require('../../assets/icon_selected.png')}/>
+                            </View>
+
+                            {/**add card charge */}
+                            <View style={styles.bank_list}>
+                                <Text onPress={this.addCard.bind(this)} style={styles.mask_event}></Text>
+                                <View style={styles.bank_left}>
+                                    <Image style={styles.bankImg_list} source={require('../../assets/icon_tjyhk.png')} />
+                                    <View>
+                                        <Text style={styles.bank_name_number}>添加银行卡支付</Text>
+                                        <Text style={styles.bank_des}>该交易只支持储蓄卡</Text>
+                                    </View>
+                                </View>
+                                <Image style={styles.arrow_icon} source={require('../../assets/right-arrow.png')}/>
+                            </View>
+                        </ScrollView>
+                    </View>
+
+                </Modal>
             </View>
         )
     }
--- src/pages/repayment/styleCss.js
+++ src/pages/repayment/styleCss.js
@@ -48,5 +48,98 @@ export const styles = StyleSheet.create({
         color: '#000117',
         fontWeight: '300',
         marginLeft: 10
+    },
+    mask_content: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        left: 0,
+        top: 0,
+        backgroundColor: '#000',
+        opacity: 0.65,
+        zIndex: 10
+    },
+    popBox: {
+        position: 'absolute',
+        left: 0,
+        bottom: 0,
+        width: '100%',
+        height: '50%',
+        backgroundColor: '#fff',
+        zIndex: 11
+    },
+    sel_nav: {
+        position: 'relative',
+        height: 48,
+        backgroundColor: '#f5f5f5'
+    },
+    closeBtn: {
+        position: 'absolute',
+        right: 19,
+        top: 19,
+        zIndex: 13
+    },
+    selImg: {
+        width: 18,
+        height: 18
+    },
+    text1: {
+        height: 48,
+        lineHeight: 48,
+        textAlign: 'center',
+        fontSize: 20,
+        color: '#000118',
+        fontWeight: '300'
+    },
+    bank_list: {
+        position: 'relative',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginLeft: 20,
+        paddingBottom: 14,
+        marginRight: 20,
+        paddingTop: 14,
+        backgroundColor: '#fff',
+        borderBottomWidth: 1,
+        borderColor: '#f5f5f5',
+        borderStyle: 'solid'
+    },
+    bankImg_list: {
+        width: 32,
+        height: 32,
+        marginRight: 9,
+        marginTop: 2
+    },
+    bank_left: {
+        flexDirection: 'row'
+    },
+    bank_name_number: {
+        color: '#000117',
+        fontSize: 14,
+        lineHeight: 20,
+        marginBottom: 1
+    },
+    bank_des: {
+        color: '#909090',
+        fontSize: 12,
+        lineHeight: 17
+    },
+    select_icon: {
+        width: 21,
+        height: 20,
+        marginTop: 10
+    },
+    arrow_icon: {
+        width: 14,
+        height: 14,
+        marginTop: 12
+    },
+    mask_event: {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: 5
     }
 });
